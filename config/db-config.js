f33d4ed24c2d1b2402ddf0eca9e342e953754053
tsaniamagistra <123210090@student.upnyk.ac.id>
2024-05-19 22:52:35 +0700

    edit connection string

--- config/db-config.js
+++ config/db-config.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
+const client = new SecretManagerServiceClient();
+const name = 'projects/926900016822/secrets/mongodb-connection-string/versions/latest';
+
+async function getSecret() {
+  try {
+    const [version] = await client.accessSecretVersion({ name: name });
+    return version.payload.data.toString('utf8');
+  } catch (err) {
+    console.error('Error retrieving secret:', err);
+    throw err;
+  }
+}
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/medication_tracker', {
+    const connectionString = await getSecret();
+    await mongoose.connect(connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
